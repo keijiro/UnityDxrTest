10dbec31caf14ba2d82b2f96f2d64fc792923b57
Keijiro Takahashi <keijiro@gmail.com>
2023-08-16 12:56:06 +0900

    Update transforms in instancing benchmark

--- Assets/Instancing/InstancingBenchmark.cs
+++ Assets/Instancing/InstancingBenchmark.cs
@@ -5,17 +5,37 @@ namespace UnityDxrTest.Instancing {
 
 public sealed class InstancingBenchmark : MonoBehaviour
 {
-    [field:SerializeField] GameObject Prefab { get; set; }
-    [field:SerializeField] Vector3Int Dimensions { get; set; } = new Vector3Int(8, 8, 8);
-    [field:SerializeField] float Interval { get; set; } = 1;
-    [field:SerializeField] bool EnableSrpBatcher { get; set; } = true;
+    #region Public properties
+
+    [field:SerializeField]
+    public GameObject Prefab { get; set; }
+
+    [field:SerializeField]
+    public Vector3Int Dimensions { get; set; } = new Vector3Int(8, 8, 8);
+
+    [field:SerializeField]
+    public float Interval { get; set; } = 1;
+
+    [field:SerializeField]
+    public bool EnableSrpBatcher { get; set; } = true;
+
+    #endregion
+
+    #region Local arrays
 
     List<GameObject> _instances = new List<GameObject>();
+    List<Transform> _xforms = new List<Transform>();
 
-    void Start()
+    #endregion
+
+    #region Private properties and methods
+
+    int TotalInstanceCount
+      => Dimensions.x * Dimensions.y * Dimensions.z;
+
+    void UpdateXforms()
     {
-        var q = Quaternion.identity;
-        var mpb = new MaterialPropertyBlock();
+        var count = 0;
         for (var i = 0u; i < Dimensions.x; i++)
         {
             var x = Interval * (i - 0.5f * (Dimensions.x - 1));
@@ -25,22 +45,49 @@ public sealed class InstancingBenchmark : MonoBehaviour
                 for (var k = 0u; k < Dimensions.z; k++)
                 {
                     var z = Interval * (k - 0.5f * (Dimensions.z - 1));
-                    var p = new Vector3(x, y, z);
-                    var go = Instantiate(Prefab, p, q);
-                    go.hideFlags = HideFlags.HideAndDontSave;
-                    if (!EnableSrpBatcher)
-                        go.GetComponent<Renderer>().SetPropertyBlock(mpb);
-                    _instances.Add(go);
+                    _xforms[count++].position = new Vector3(x, y, z);
                 }
             }
         }
     }
 
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void Start()
+    {
+        var sheet = new MaterialPropertyBlock();
+
+        _instances.Capacity = _xforms.Capacity = TotalInstanceCount;
+
+        for (var i = 0u; i < TotalInstanceCount; i++)
+        {
+            var go = Instantiate(Prefab);
+
+            go.hideFlags = HideFlags.HideAndDontSave;
+
+            if (!EnableSrpBatcher)
+                go.GetComponent<Renderer>().SetPropertyBlock(sheet);
+
+            _instances.Add(go);
+            _xforms.Add(go.transform);
+        }
+
+        UpdateXforms();
+    }
+
+    void Update()
+      => UpdateXforms();
+
     void OnDestroy()
     {
         foreach (var go in _instances) Destroy(go);
         _instances.Clear();
+        _xforms.Clear();
     }
+
+    #endregion
 }
 
 } // namespace UnityDxrTest.Instancing
