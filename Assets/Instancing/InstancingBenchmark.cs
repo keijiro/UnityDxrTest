8e085d565e06457666ed4c3c8f52b606f188bda1
Keijiro Takahashi <keijiro@gmail.com>
2023-08-17 10:38:23 +0900

    cleaning up

--- Assets/Instancing/InstancingBenchmark.cs
+++ Assets/Instancing/InstancingBenchmark.cs
@@ -1,6 +1,8 @@
+using System.Linq;
+using Unity.Burst;
+using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Profiling;
-using System.Collections.Generic;
+using UnityEngine.Jobs;
 
 namespace UnityDxrTest.Instancing {
 
@@ -12,7 +14,7 @@ public sealed class InstancingBenchmark : MonoBehaviour
     public GameObject Prefab { get; set; }
 
     [field:SerializeField]
-    public Vector3Int Dimensions { get; set; } = new Vector3Int(8, 8, 8);
+    public int3 Dimensions { get; set; } = 8;
 
     [field:SerializeField]
     public float Interval { get; set; } = 1;
@@ -24,49 +26,19 @@ public sealed class InstancingBenchmark : MonoBehaviour
 
     #region Local arrays
 
-    List<GameObject> _instances = new List<GameObject>();
-    List<Transform> _xforms = new List<Transform>();
-    List<Vector4> _temp = new List<Vector4>();
+    GameObject[] _instances;
 
     #endregion
 
     #region Private properties and methods
 
+    TransformAccessArray _xforms;
+
     int TotalInstanceCount
       => Dimensions.x * Dimensions.y * Dimensions.z;
 
-    void UpdateTemp()
-    {
-        var count = 0;
-        for (var i = 0u; i < Dimensions.x; i++)
-        {
-            var x = Interval * (i - 0.5f * (Dimensions.x - 1));
-            for (var j = 0u; j < Dimensions.y; j++)
-            {
-                var y = Interval * (j - 0.5f * (Dimensions.y - 1));
-                for (var k = 0u; k < Dimensions.z; k++)
-                {
-                    var z = Interval * (k - 0.5f * (Dimensions.z - 1));
-                    _temp[count++] = new Vector4(x, y, z, 0.1f);
-                }
-            }
-        }
-    }
-
     void UpdateXforms()
-    {
-        Profiler.BeginSample("Update Temp");
-        UpdateTemp();
-        Profiler.EndSample();
-
-        Profiler.BeginSample("Update Transforms");
-        for (var i = 0; i < TotalInstanceCount; i++)
-        {
-            _xforms[i].localPosition = _temp[i];
-            _xforms[i].localScale = Vector3.one * _temp[i].w;
-        }
-        Profiler.EndSample();
-    }
+      => new XformJob(){ Dimensions = Dimensions }.Schedule(_xforms);
 
     #endregion
 
@@ -75,22 +47,21 @@ public sealed class InstancingBenchmark : MonoBehaviour
     void Start()
     {
         var sheet = new MaterialPropertyBlock();
-
-        _instances.Capacity = _xforms.Capacity = TotalInstanceCount;
-        _temp.AddRange(new Vector4[TotalInstanceCount]);
+        _instances = new GameObject[TotalInstanceCount];
 
         for (var i = 0; i < TotalInstanceCount; i++)
         {
             var go = Instantiate(Prefab);
-
             go.hideFlags = HideFlags.HideAndDontSave;
 
             if (!EnableSrpBatcher)
                 go.GetComponent<Renderer>().SetPropertyBlock(sheet);
 
-            _instances.Add(go);
-            _xforms.Add(go.transform);
+            _instances[i] = go;
         }
+
+        _xforms = new TransformAccessArray
+          (_instances.Select(go => go.transform).ToArray());
 
         UpdateXforms();
     }
@@ -101,11 +72,27 @@ public sealed class InstancingBenchmark : MonoBehaviour
     void OnDestroy()
     {
         foreach (var go in _instances) Destroy(go);
-        _instances.Clear();
-        _xforms.Clear();
+        _instances = null;
+        _xforms.Dispose();
     }
 
     #endregion
+}
+
+[BurstCompile]
+struct XformJob : IJobParallelForTransform
+{
+    public int3 Dimensions;
+
+    [BurstCompile]
+    public void Execute(int index, TransformAccess transform)
+    {
+        var p = math.float3(index % Dimensions.x,
+                            index / Dimensions.x % Dimensions.y,
+                            index / (Dimensions.x * Dimensions.y));
+        transform.position = (p - (float3)(Dimensions - 1) * 0.5f) * 0.1f;
+        transform.localScale = (float3)0.1f;
+    }
 }
 
 } // namespace UnityDxrTest.Instancing
